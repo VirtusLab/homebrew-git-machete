156bf22a995fb84ccb9d69cf890d6a2693bf68ef
Pawel Lipski <pawel.p.lipski@gmail.com>
2019-12-23 16:18:29 +0100

    Release 2.12.6

---
+++ git-machete.rb
@@ -0,0 +1,30 @@
+class GitMachete < Formula
+  include Language::Python::Virtualenv
+  desc "Git repository organizer & rebase workflow automation tool"
+  homepage "https://github.com/VirtusLab/git-machete"
+  url "https://pypi.io/packages/source/g/git-machete/git-machete-2.12.6.tar.gz"
+  sha256 "86cfbd32d66c193302b0b27993b4c5d5a1a9b2d1c338f133b6f4024d8e53edc0"
+  depends_on "python"
+
+  def install
+    virtualenv_install_with_resources
+
+    bash_completion.install "completion/git-machete.completion.bash"
+    zsh_completion.install "completion/git-machete.completion.zsh"
+  end
+
+  test do
+    system "git", "init"
+    (testpath/"test").write "foo"
+    system "git", "add", "test"
+    system "git", "commit", "--message", "Initial commit"
+    system "git", "checkout", "-b", "develop"
+    (testpath/"test2").write "bar"
+    system "git", "add", "test2"
+    system "git", "commit", "--message", "Other commit"
+
+    (testpath/".git/machete").write "master\n  develop"
+    expected_output = "  master\n  | \n  | Other commit\n  o-develop *\n"
+    assert_match expected_output, shell_output("git machete status --list-commits")
+  end
+end
