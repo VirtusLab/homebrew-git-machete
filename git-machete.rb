d200c881f7165655471945c4ea2946796cbdfa93
Travis CI <travis@travis-ci.org>
2020-08-26 07:56:29 +0000

    Release 2.15.4, Travis build: 1289

--- git-machete.rb
+++ git-machete.rb
@@ -2,8 +2,8 @@ class GitMachete < Formula
   include Language::Python::Virtualenv
   desc "Git repository organizer & rebase workflow automation tool"
   homepage "https://github.com/VirtusLab/git-machete"
-  url "https://pypi.io/packages/source/g/git-machete/git-machete-2.15.3.tar.gz"
-  sha256 "4ba254151cbfe8f7e1d8891ef22902b526a6078d321c6febd9c75a197888ee4e"
+  url "https://pypi.io/packages/source/g/git-machete/git-machete-2.15.4.tar.gz"
+  sha256 "35d26f02f7c56b762a202491cd02b1939bc4c264d07950ccd98ebaa1a5ce5458"
   depends_on "python"
 
   def install
